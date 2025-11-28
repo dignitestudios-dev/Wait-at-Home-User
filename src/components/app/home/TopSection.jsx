1e6fa1c299a2347929fd33ca4a136c58c484c055
mathewgarfield <189109425+mathewgarfield@users.noreply.github.com>
2025-11-28 06:23:05 -0800

    Update :Remove wait at home

--- src/components/app/home/TopSection.jsx
+++ src/components/app/home/TopSection.jsx
@@ -8,10 +8,10 @@ const TopSection = () => {
   const { userData } = useContext(AppContext);
   return (
     <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center gap-4   rounded-xl">
-       <div className="flex items-center gap-3 ">
+       {/* <div className="flex items-center gap-3 ">
         <img src={LogoWhite} className="w-[40px]" alt="" />
         <h2 className="text-[24px] font-[600] text-white mb-2">Wait At Home</h2>
-      </div>
+      </div> */}
       {userData?.isUserRegistered && <ChatAndNotiBtn />}
     </div>
   );
