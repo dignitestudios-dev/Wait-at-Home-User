8f91734b61b3488dc57ae15fa38a4d184767b17f
mathewgarfield <189109425+mathewgarfield@users.noreply.github.com>
2026-02-11 05:38:59 -0800

    code updated

--- src/components/app/home/TopSection.jsx
+++ src/components/app/home/TopSection.jsx
@@ -11,7 +11,7 @@ const TopSection = () => {
       <div className="flex items-center gap-3 ">
         {/* <img src={LogoWhite} className="w-[40px]" alt="" /> */}
         <h2 className=" text-xl  font-semibold text-[#684D7B]">
-          Welcome to Wait at Home™ — the virtual waiting experience designed for pets and their families.
+          Welcome to Wait at Home™ — the virtual waiting experience designed for pets and their families. <br />
           Reserve your place in line without sitting in a busy lobby. <br /> We will send real-time text and email updates so you know when to arrive with your pet.
 
         </h2>
