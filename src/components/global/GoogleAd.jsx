994f06680ad7a2d35ae6c3df8cd710ede652f10d
kevinparker-dev <142620931+dignitestudios-dev@users.noreply.github.com>
2026-09-09 19:25:45 +0500

    feat: add GoogleAd component and update AdSense publisher ID in index.html

--- src/components/global/GoogleAd.jsx
+++ src/components/global/GoogleAd.jsx
@@ -18,7 +18,7 @@ const GoogleAd = ({ dataAdSlot }) => {
       data-ad-slot={dataAdSlot}
       data-ad-format="auto"
       data-full-width-responsive="true"
-      data-adtest="on"
+      // data-adtest="on"
     ></ins>
   );
 };
